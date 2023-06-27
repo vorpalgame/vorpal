bd6b0ef671aa9a996483d3d76636df0900a909ec
Ranx0r0x <register@bradleejohnson.com>
2023-06-27 14:46:36 -0500

    Adding renderer test and beginning refactot to composable elements.

--- samples/lib/image_renderer.go
+++ samples/lib/image_renderer.go
@@ -6,8 +6,9 @@ import (
 	"github.com/vorpalgame/vorpal/bus"
 )
 
+// TODO We need to refactor to decompose into pipeline segments that are composable.
 func NewImageRenderer() ImageRenderer {
-	return &imageControllerData{}
+	return &imageRendererData{}
 }
 
 type ImageRenderer interface {
@@ -17,25 +18,26 @@ type ImageRenderer interface {
 	IncrementScale(percent int32) ImageRenderer
 }
 
-type imageControllerData struct {
+type imageRendererData struct {
 	imageName string
 	scale     int32
 }
 
-func (s *imageControllerData) SetImageName(imageName string) ImageRenderer {
+func (s *imageRendererData) SetImageName(imageName string) ImageRenderer {
 	s.imageName = imageName
 	return s
 }
-func (s *imageControllerData) IncrementScale(percent int32) ImageRenderer {
+func (s *imageRendererData) IncrementScale(percent int32) ImageRenderer {
 	s.scale += percent
 	return s
 }
-func (s *imageControllerData) SetScale(percent int32) ImageRenderer {
+func (s *imageRendererData) SetScale(percent int32) ImageRenderer {
 	s.scale = percent
 	return s
 }
 
-func (s *imageControllerData) CreateImageLayer(mouseEvent bus.MouseEvent, fd FrameData, cl CurrentLocation) bus.ImageLayer {
+// TODO This is coupled to the frame data flip book style animation and shouldn't be.
+func (s *imageRendererData) CreateImageLayer(mouseEvent bus.MouseEvent, fd FrameData, cl CurrentLocation) bus.ImageLayer {
 	imgData := bus.NewImageMetadata(fmt.Sprintf(s.imageName, fd.GetCurrentFrame()), cl.GetX(), cl.GetY(), s.scale)
 	imgData.SetFlipHorizontal(s.flipHorizontal(mouseEvent, cl))
 	layer := bus.NewImageLayer().AddLayerData(imgData)
@@ -44,9 +46,9 @@ func (s *imageControllerData) CreateImageLayer(mouseEvent bus.MouseEvent, fd Fra
 	return layer
 }
 
-func (s *imageControllerData) flipHorizontal(mouseEvent bus.MouseEvent, cl CurrentLocation) bool {
+func (s *imageRendererData) flipHorizontal(mouseEvent bus.MouseEvent, cl CurrentLocation) bool {
 	return mouseEvent.GetX() < cl.GetX()
 }
-func (s *imageControllerData) getScale() int32 {
+func (s *imageRendererData) getScale() int32 {
 	return s.scale
 }
